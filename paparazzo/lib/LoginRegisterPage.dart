b98247a6d3757f115a941a0e25075c7ee8f64b3d
Bharat Shrivastava <bharatsdruv@gmail.com>
2019-10-01 01:08:03 +0530

    Added LoginPage Design

---
+++ paparazzo/lib/LoginRegisterPage.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+class LoginRegisterPage extends StatefulWidget 
+{
+  State<StatefulWidget> createState()
+  {
+    return _LoginRegisterState();
+  }
+  
+}
+
+class _LoginRegisterState extends State<LoginRegisterPage> 
+{
+  //methods
+  void validateAndSave()
+  {
+
+  }
+
+  void moveToRegister()
+  {
+
+  }
+
+  //Design
+  @override
+  Widget build(BuildContext context) 
+  {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("paparazzo"),
+      ),
+      body: new Container(
+        margin: EdgeInsets.all(15.0),
+        child: new Form(
+          child: new Column
+            (
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: createInputs()+createButton(),
+
+          ),
+        ),
+      ),
+    );
+  }
+  List<Widget> createInputs()
+  {
+    return 
+        [
+          SizedBox(height: 10.0,),
+          logo(),
+          SizedBox(height: 20.0,),
+
+          new TextFormField(
+            decoration: new InputDecoration(labelText: 'Email'),
+          ),
+          SizedBox(height: 10.0,),
+          new TextFormField(
+            decoration: new InputDecoration(labelText: 'Password'),
+          ),
+          SizedBox(height: 20.0,),
+        ];
+  }
+  Widget logo()
+  {
+    return new Hero
+    (
+      tag: 'hero',
+      child: new CircleAvatar(
+        backgroundColor: Colors.transparent,
+        radius: 110.0,
+        child: Image.asset('images/P.png'),
+      ),
+    );
+  }
+
+  List<Widget> createButton()
+  {
+    return[
+      new RaisedButton
+      (
+        child: new Text('Login',style: new TextStyle(fontSize: 20.0),),
+        textColor: Colors.white,
+        color: Colors.deepPurple,
+        onPressed: validateAndSave,
+      ),
+
+      new FlatButton
+      (
+        child: new Text('Create Account?',style: new TextStyle(fontSize: 14.0),),
+        textColor: Colors.white,
+
+        onPressed: moveToRegister,
+      )
+
+    ];
+  }
+}
---
+++ paparazzo/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:paparazzo/LoginRegisterPage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context)
+  {
+    return new MaterialApp(
+      title: 'paparazzo',
+      theme: new ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: LoginRegisterPage(),
+    );
+  }
+}
